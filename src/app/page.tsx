aa6935932d79e49ca3d20589937ca37e8e74ab35
Ali Mammadli <nfsavss@icloud.com>
2024-12-18 11:16:18 +0400

    Fixed responsiveness for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,24 @@ export default function Home() {
     return (
         <main className="flex flex-col w-full items-center justify-center pb-40 gap-8 row-start-2 sm:items-start">
 
-            <div className="flex flex-col w-full h-screen justify-between py-12">
+            <div className="flex flex-col w-full h-screen justify-between pt-5 pb-20 lg:py-12">
 
                 <section className="flex flex-col w-full items-center">
-                    <div className="flex justify-between w-full px-44">
+                    <div className="flex justify-between w-full px-5 lg:px-44">
                         <div className="flex items-center">
-                            <img src='/bsu.png' className="h-28" />
+                            <img src='/bsu.png' className="h-20 lg:h-28" />
                         </div>
                         <div className="flex items-center">
-                            <img src='/sabah.svg' className="h-28" />
+                            <img src='/sabah.svg' className="h-20 lg:h-28" />
                         </div>
                     </div>
                 </section>
 
                 <section className="flex flex-col items-center w-full">
                     <div className="flex flex-col w-fit text-center">
-                        <h2 className="text-4xl font-bold tracking-widest" style={{ color: "#3c3c4399" }}>BAKI DÖVLƏT UNİVERSİTETİ</h2>
-                        <h2 className="text-6xl font-semibold mt-2 mb-5" style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>SABAH Qrupları</h2>
-                        <h2 className="font-medium" style={{ fontSize: 40, color: "#3c3c4399" }}>Kompüter Elmləri</h2>
+                        <h2 className="text-3xl lg:text-4xl font-bold tracking-widest" style={{ color: "#3c3c4399" }}>BAKI DÖVLƏT UNİVERSİTETİ</h2>
+                        <h2 className="text-4xl lg:text-6xl font-semibold mt-2 mb-5" style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>SABAH Qrupları</h2>
+                        <h2 className="text-3xl lg:text-[40px] font-medium" style={{ color: "#3c3c4399" }}>Kompüter Elmləri</h2>
                     </div>
                 </section>
 
@@ -36,39 +36,39 @@ export default function Home() {
 
 
             <section className="flex flex-col items-center w-full mt-32">
-                <div className="flex w-2/3 gap-5" style={{ height: "30vw" }}>
+                <div className="flex flex-col lg:flex-row w-full lg:w-2/3 lg:h-[30vw] gap-5 p-5">
                     <div className="flex flex-col w-full h-full gap-5">
-                        <div className="flex flex-col items-center justify-center w-full h-full p-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
+                        <div className="flex flex-col  py-16 lg:py-5 py-16 lg:py-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
                             <h1 className='font-semibold text-7xl' style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>1</h1>
                             <h3 className="text-xl leading-tight font-semibold">DAAD Təqaüdü Qalibi</h3>
                         </div>
-                        <div className="flex flex-col items-center justify-center w-full h-full p-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-5 py-16 lg:py-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
                             <h1 className='font-semibold text-7xl' style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>1</h1>
                             <h3 className="text-xl leading-tight font-semibold">Əlaçı Təqaüdü Qalibi</h3>
                         </div>
                     </div>
                     <div className="flex flex-col w-full h-full items-center gap-5">
-                        <div className="flex flex-col items-center justify-center w-full p-5 h-full rounded-xl" style={{ backgroundColor: "var(--bg-3)" }}>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-5 py-16 lg:py-5 rounded-xl" style={{ backgroundColor: "var(--bg-3)" }}>
                             <h1 className='font-semibold text-7xl' style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>487</h1>
                             <h3 className="text-2xl leading-tight font-semibold">Tələbə</h3>
                         </div>
-                        <div className="flex flex-col relative items-center justify-center w-full h-full p-5 rounded-xl overflow-hidden" style={{ backgroundColor: "var(--bg-3)" }}>
+                        <div className="flex flex-col relative items-center justify-center w-full h-full p-5 py-16 lg:py-5 rounded-xl overflow-hidden" style={{ backgroundColor: "var(--bg-3)" }}>
                             <div className='flex absolute opacity-40'>
                                 <img src="/sabah.svg" className="h-44" alt="" />
                             </div>
                             <h1 className='font-semibold text-7xl z-10' style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>SABAH</h1>
                         </div>
-                        <div className="flex flex-col items-center justify-center w-full h-full p-5 rounded-xl" style={{ backgroundColor: "var(--bg-3)" }}>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-5 py-16 lg:py-5 rounded-xl" style={{ backgroundColor: "var(--bg-3)" }}>
                             <h1 className='font-semibold text-7xl' style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>239</h1>
                             <h3 className="text-2xl leading-tight font-semibold">Məzun</h3>
                         </div>
                     </div>
                     <div className="flex flex-col w-full h-full gap-5">
-                        <div className="flex flex-col items-center justify-center w-full h-full p-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-5 py-16 lg:py-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
                             <h1 className='font-semibold text-7xl' style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>47</h1>
                             <h3 className="text-xl leading-tight font-semibold">Fərqlənmə ilə bitirən məzun</h3>
                         </div>
-                        <div className="flex flex-col items-center justify-center w-full h-full p-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
+                        <div className="flex flex-col items-center justify-center w-full h-full p-5 py-16 lg:py-5 rounded-xl text-center" style={{ backgroundColor: "var(--bg-3)" }}>
                             <h1 className='font-semibold text-7xl' style={{ backgroundImage: "var(--gradient)", color: "transparent", backgroundClip: "text" }}>648</h1>
                             <h3 className="text-xl leading-tight font-semibold">Ən yüksək qəbul balı</h3>
                         </div>
@@ -77,7 +77,7 @@ export default function Home() {
             </section>
 
             <section className="flex flex-col w-full items-center mt-32">
-                <div className="mt-20 gap-10" style={{ display: "grid", width: 1000, gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))" }}>
+                <div className="w-full lg:w-[1040px] mt-20 gap-10 p-5" style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))" }}>
                     {students.map((s, index) => {
                         return (
                             <div className="group flex flex-col relative h-256 rounded-xl overflow-hidden transition-all cursor-pointer" style={{ backgroundColor: "var(--bg-3)" }} key={index}>
@@ -98,21 +98,20 @@ export default function Home() {
             </section>
 
             <section className="flex flex-col w-full items-center mt-48">
-                <div className="flex flex-col gap-14" style={{ width: 1000 }}>
+                <div className="flex flex-col w-full lg:w-[1000px] gap-14 p-5">
                     {teams.map((team, index) => {
                         return (
-                            <div className="flex h-1/4 gap-10 p-5 rounded-xl" style={{ backgroundColor: "var(--bg-3)" }} key={index}>
-                                <div className="flex flex-col w-2/3">
+                            <div className="flex flex-col lg:flex-row lg:h-1/4 gap-10 p-5 rounded-xl" style={{ backgroundColor: "var(--bg-3)" }} key={index}>
+                                <div className="flex flex-col lg:w-2/3">
                                     <h4 className="text-2xl font-semibold">{team.title}</h4>
                                     <h6 className="text-sm font-normal mt-4 whitespace-pre-wrap">{team.details}</h6>
                                 </div>
-                                <div className="flex flex-col w-1/3 gap-5 h-96 overflow-y-scroll">
+                                <div className="flex flex-col lg:w-1/3 gap-5 lg:h-96 overflow-y-scroll">
                                     {team.images.map((img, index) => <img src={`/teams/${img}`} className="w-full rounded-md" alt="" key={index} />)}
                                 </div>
                             </div>
                         )
                     })}
-
                 </div>
             </section>
 
